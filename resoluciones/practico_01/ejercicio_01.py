dfde79dce74ed1428917f6bfc4d63d3a8ea1db0b
Valentin0804 <valenmoli_18@hotmail.com>
2024-03-26 17:53:26 -0300

    Agrego carpetas de resolucion, y resolucion del ejercicio_01.py de la practica_01

---
+++ resoluciones/practico_01/ejercicio_01.py
@@ -0,0 +1,21 @@
+def maximo_basico(a: float, b: float) -> float:
+    if a > b:
+        return a
+    else:
+        return b
+
+assert maximo_basico(10, 5) == 10
+assert maximo_basico(9, 18) == 18
+
+
+def maximo_libreria(a: float, b: float) -> float:
+  return max(a, b)
+
+assert maximo_libreria(10, 5) == 10
+assert maximo_libreria(9, 18) == 18
+
+def maximo_ternario(a: float, b: float) -> float:
+    return a if a >= b else b
+
+assert maximo_ternario(10, 5) == 10
+assert maximo_ternario(9, 18) == 18
