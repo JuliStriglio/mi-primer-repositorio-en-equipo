434bea97f7f589d5cf5c914748f478abaa0c81e9
Juli Striglio <juliss2001@hotmail.com>
2024-03-27 16:09:59 -0300

    Completo resolucion 06/11

--- resoluciones/practico_01/ejercicio_06.py
+++ resoluciones/practico_01/ejercicio_06.py
@@ -1,3 +1,4 @@
+
 from typing import List, Union
 
 def numeros_al_final_basico(lista: List[Union[float, str]]) -> List[Union[float, str]]:
@@ -30,3 +31,21 @@ assert numeros_al_final_comprension([3, "a", 1, "b", 10, "j"]) == ["a", "b", "j"
 
 #########################################################################################
 
+def numeros_al_final_sorted(lista: List[Union[float, str]]) -> List[Union[float, str]]:
+
+    return sorted(lista, key=lambda l: not isinstance(l, (int,float))) #arreglarlo
+
+assert numeros_al_final_sorted([3, "a", 1, "b", 10, "j"]) == ["a", "b", "j", 3, 1, 10]
+
+
+#########################################################################################
+
+def numeros_al_final_filter(lista: List[Union[float, str]]) -> List[Union[float, str]]:
+
+    enteros = list(filter(lambda l: isinstance(l,(int,float)), lista))
+    strings = list(filter(lambda l : not isinstance(l,(int,float)), lista))
+
+    return strings + enteros
+
+if __name__ == "__main__":
+    assert numeros_al_final_filter([3, "a", 1, "b", 10, "j"]) == ["a", "b", "j", 3, 1, 10]
--- resoluciones/practico_01/ejercicio_11.py
+++ resoluciones/practico_01/ejercicio_11.py
@@ -2,13 +2,47 @@ from typing import Iterable
 
 def suma_cubo_pares_for(numeros: Iterable[int]) -> int:
 
+    cubos=[]
     suma = 0
     for n in numeros:
         cubo = n**3
-    for n in numeros:
-        if n % 2 == 0:
-              suma +=n
+        cubos.append(cubo)
+
+    for m in cubos:
+        if m % 2 == 0:
+            suma = suma + m
 
     return suma
 
 assert suma_cubo_pares_for([1, 2, 3, 4, 5, 6]) == 288
+
+
+###############################################################################
+
+def suma_cubo_pares_sum_list(numeros: Iterable[int]) -> int:
+     return sum(s ** 3 for s in numeros if s % 2 == 0)
+
+assert suma_cubo_pares_for([1, 2, 3, 4, 5, 6]) == 288
+
+
+###############################################################################
+
+    #Re-Escribir utilizando expresiones generadoras (debe resolverse en 1 línea)
+   # y la función sum.
+
+
+
+
+###############################################################################
+
+numeros = [1, 2, 3, 4, 5, 6]
+
+numeros_al_cubo = list(map(lambda l: l ** 3, numeros))
+numeros_al_cubo_pares = list(map(lambda n: n**3, filter(lambda l: l % 2 ==0, numeros)))
+suma_numeros_al_cubo_pares = sum(map(lambda n: n**3, filter(lambda l: l % 2 ==0, numeros)))
+numeros_ordenada = sorted(numeros, key=lambda l: l % 2 ==0)
+
+assert numeros_al_cubo == [1, 8, 27, 64, 125, 216]
+assert numeros_al_cubo_pares == [8, 64, 216]
+assert suma_numeros_al_cubo_pares == 288
+assert numeros_ordenada == [1, 3, 5, 2, 4, 6]
