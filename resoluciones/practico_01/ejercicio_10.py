f891a3d2ef9b7e05b132eda5935c7691c3839d08
Lucas Viguera <123002129+LucasViguera@users.noreply.github.com>
2024-04-10 19:06:25 -0300

    Agrego ejercicio_10.py

---
+++ resoluciones/practico_01/ejercicio_10.py
@@ -0,0 +1,56 @@
+"""For-Else, Any."""
+
+from typing import Iterable
+
+def tiene_pares_basico(numeros: Iterable[int]) -> bool:
+    """Toma una lista y devuelve un booleano en función si tiene al menos un
+    número par."""
+    i = 0
+    tiene_pares_basico = False
+    while i < len(numeros) and not tiene_pares_basico:
+        tiene_pares_basico = numeros[i] % 2 == 0
+        i += 1
+    return tiene_pares_basico
+
+# NO MODIFICAR - INICIO
+assert tiene_pares_basico([1, 3, 5]) is False
+assert tiene_pares_basico([1, 3, 5, 6]) is True
+assert tiene_pares_basico([1, 3, 5, 600]) is True
+# NO MODIFICAR - FIN
+
+
+###############################################################################
+
+
+def tiene_pares_for_else(numeros: Iterable[int]) -> bool:
+    """Re-Escribir utilizando for-else con dos return y un break.
+    Referencia: https://docs.python.org/3/tutorial/controlflow.html#break-and-continue-statements-and-else-clauses-on-loops
+    """
+    for numero in numeros:
+        if numero % 2 == 0:
+            return True
+    else:
+        return False
+    
+
+# NO MODIFICAR - INICIO
+assert tiene_pares_for_else([1, 3, 5]) is False
+assert tiene_pares_for_else([1, 3, 5, 6]) is True
+assert tiene_pares_for_else([1, 3, 5, 600]) is True
+# NO MODIFICAR - FIN
+
+
+###############################################################################
+
+
+def tiene_pares_any(numeros: Iterable[int]) -> bool:
+    """Re-Escribir utilizando la función any, sin utilizar bucles.
+    Referencia: https://docs.python.org/3/library/functions.html#any
+    """
+    return any (numero % 2 == 0 for numero in numeros)
+
+# NO MODIFICAR - INICIO
+assert tiene_pares_any([1, 3, 5]) is False
+assert tiene_pares_any([1, 3, 5, 6]) is True
+assert tiene_pares_any([1, 3, 5, 600]) is True
+# NO MODIFICAR - FIN
